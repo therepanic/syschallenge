120714596cdeb03106967b5f863d27de7c0058c3
Andrey Litvitski <andrey1010102008@gmail.com>
2025-03-01 19:13:59 +0300

    add basic StorageService interface through AWS

---
+++ domain-service/src/main/java/com/syschallenge/shared/configuration/AwsS3Configuration.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright by the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.syschallenge.shared.configuration;
+
+import com.syschallenge.shared.property.AwsS3Property;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+/**
+ * Configuration class for AWS S3 client setup
+ *
+ * @author panic08
+ * @since 1.0.0
+ */
+@Configuration
+@RequiredArgsConstructor
+public class AwsS3Configuration {
+
+    private final AwsS3Property awsS3Property;
+
+    /**
+     * Creates and configures an {@link S3Client} bean.
+     *
+     * @return the configured {@link S3Client} instance
+     */
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(awsS3Property.region()))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(
+                                awsS3Property.accessKey(),
+                                awsS3Property.secretKey()
+                        )
+                ))
+                .endpointOverride(URI.create(awsS3Property.endpoint()))
+                .serviceConfiguration(software.amazon.awssdk.services.s3.S3Configuration.builder()
+                        .pathStyleAccessEnabled(true)
+                        .build())
+                .build();
+    }
+
+}
